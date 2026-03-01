c3d8ff75444824c37f801a21744d9a59863c17c9
Toby Kerr <toby@tobykerr.com>
2026-03-01 11:48:38 +0000

    Reorganise files

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import random
 import database
-import cases
+
+# create container class
+class container:
+    def __init__(self, caseType, name, cost, itemDict, stat, minQ, maxQ):
+        self.name = f"{name.capitalize} {caseType.capitalize}"
+        self.type
+        self.cost = cost
+        self.items = itemDict
+        self.stat = stat
+        self.minQ = minQ
+        self.maxQ = maxQ
+
+# test = container("case", "test", 1.5, {2 : [1, 49], 3 : [2], 4 : [111], 5 : [1111], 6 : [69]}, 1, 2, 6)
