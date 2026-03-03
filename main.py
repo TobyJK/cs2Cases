dc5d992b9ca1cd35a9214f1d10fd27a6d0f32b42
Toby Kerr <toby@tobykerr.com>
2026-03-03 00:16:46 +0000

    Remove need for custom skin class

--- main.py
+++ main.py
@@ -14,16 +14,6 @@ class container:
         self.minQ = minQ
         self.maxQ = maxQ
 
-# create skin class
-class skin:
-    def __init__(self, id, name, quality, minFloat, maxFloat, hasPattern):
-        self.id = id
-        self.name = name
-        self.quality = quality
-        self.minFloat = minFloat
-        self.maxFloat = maxFloat
-        self.hasPattern = hasPattern
-
 # test = container("case", "test", 1.5, {2 : [1, 49], 3 : [2], 4 : [111], 5 : [1111], 6 : [69]}, 1, 2, 6)
 
 skins = pd.DataFrame.from_dict(database.skins, orient='index')
@@ -32,9 +22,6 @@ collections = pd.DataFrame.from_dict(database.collections, orient='index')
 cases = pd.DataFrame.from_dict(database.cases, orient='index')
 collectionCases = pd.DataFrame.from_dict(database.collectionCases, orient='index')
 
-def createSkinObject(row):
-    return skin(row.name, row["name"], row["quality"], row["minFloat"], row["maxFloat"], row["hasRarePattern"])
-
 def getSkinsFromCase(name):
     cNumber = cases[cases["name"] == name].index[0]
     col = collectionCases.loc[collectionCases["caseid"] == cNumber]["collectionid"].iloc[0]
@@ -42,5 +29,4 @@ def getSkinsFromCase(name):
     statOrSouv = "stat" if s["stattrak"].iloc[0] else "souv" if s["souvenir"].iloc[0] else False
     s = s[["name", "quality", "minFloat", "maxFloat", "hasRarePattern"]]
 
-    return s.apply(createSkinObject, axis=1), statOrSouv
-
+    return s, statOrSouv
